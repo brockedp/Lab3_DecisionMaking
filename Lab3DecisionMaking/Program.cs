6187c5c6349555ec1bdad3fbefff4bce7ea584f6
Deno <denobrockert@yahoo.com>
2019-07-16 19:10:55 -0400

    First push for Lab3 Decision Making

---
+++ Lab3DecisionMaking/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Lab3DecisionMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string userName = Console.ReadLine();
+            bool oddEven = true;
+            while (oddEven)
+            {
+                // Input
+                
+                Console.WriteLine("Enter a number from 1 to 100 please");
+                string userInput = Console.ReadLine();
+                int number = int.Parse(userInput);
+
+                //Processing & output
+                if ((number % 2 != 0) && (number < 60) && (number > 0))
+                {
+                    Console.WriteLine($"{userName}, {number} is odd");
+                }
+                else if ((number % 2 == 0) && (number >= 2) && (number <= 25))
+                {
+                    Console.WriteLine($"{userName}, {number} is even and less than 25");
+                }
+                else if ((number % 2 == 0) && (number >= 26) && (number <= 60))
+                {
+                    Console.WriteLine($"{userName}, the result is even");
+                }
+                else if ((number % 2 == 0) && (number > 60) && (number <= 100))
+                {
+                    Console.WriteLine($"{userName}, {number} is even");
+                }
+                else if ((number % 2 != 0) && (number > 60) && (number <= 100))
+                {
+                    Console.WriteLine($"{userName}, {number} is odd");
+                }
+                else
+                {
+                    Console.WriteLine($"{userName}, I'm sorry that number is not between 1 to 100");
+                }
+                bool tryAgain = true;
+                while (tryAgain)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"{userName}, would you like to try again? y or n");
+                    string userAnswer = Console.ReadLine().ToLower();
+                    switch (userAnswer)
+                    {
+                        case "y":
+                            tryAgain = false;
+                            break;
+                        case "n":
+                            oddEven = false;
+                            tryAgain = false;
+                            break;
+                        default:
+                            Console.WriteLine($"{userName}, that could not be computed. Please type y for yes or n for no");
+                            break;
+                    }
+
+                }
+            }
+            Console.WriteLine("Goodbye");
+
+
+
+        }
+    }
+
+}
